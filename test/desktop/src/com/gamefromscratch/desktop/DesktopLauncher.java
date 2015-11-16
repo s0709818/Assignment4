66e02062c118f016b13c3bbf36640594522d8b07
s0709818 <s0709818@monmouth.edu>
2015-11-16 16:02:59 -0500

    Assignment 4
    Am I doing this right even?

---
+++ test/desktop/src/com/gamefromscratch/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamefromscratch.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamefromscratch.Physics1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Physics1(), config);
+	}
+}
